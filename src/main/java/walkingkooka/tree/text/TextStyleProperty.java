041ad60f645e2a31aafcf2857b5b2a7e1547d7e5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-20 16:40:10 +1000

    TextStyleProperty

---
+++ src/main/java/walkingkooka/tree/text/TextStyleProperty.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.text;
+
+import walkingkooka.Cast;
+import walkingkooka.ToStringBuilder;
+import walkingkooka.UsesToStringBuilder;
+import walkingkooka.Value;
+import walkingkooka.naming.HasName;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintable;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A pair that captures a {@link TextStylePropertyName} and an optional value for that property.
+ */
+public final class TextStyleProperty<T> implements HasName<TextStylePropertyName<T>>,
+        Value<Optional<T>>,
+        TreePrintable,
+        UsesToStringBuilder {
+
+    public static <T> TextStyleProperty<T> with(final TextStylePropertyName<T> name,
+                                                final Optional<T> value) {
+        return new TextStyleProperty<T>(
+                Objects.requireNonNull(name, "name"),
+                Objects.requireNonNull(value, "value")
+        );
+    }
+
+    private TextStyleProperty(final TextStylePropertyName<T> name,
+                              final Optional<T> value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public TextStylePropertyName<T> name() {
+        return this.name;
+    }
+
+    private final TextStylePropertyName<T> name;
+
+    @Override
+    public Optional<T> value() {
+        return this.value;
+    }
+
+    private final Optional<T> value;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.name,
+                this.value
+        );
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+                other instanceof TextStyleProperty &&
+                        this.equals0(Cast.to(other));
+    }
+
+    private boolean equals0(final TextStyleProperty other) {
+        return this.name.equals(other.name) &&
+                this.value.equals(other.value);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.buildFrom(this);
+    }
+
+    // UsesToStringBuilder.............................................................................................
+
+    @Override
+    public void buildToString(final ToStringBuilder builder) {
+        builder.label(this.name.value())
+                .value(this.value);
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        printer.println(this.name.value());
+
+        {
+            final Optional<T> value = this.value;
+            if(value.isPresent()) {
+                printer.indent();
+                {
+                    TreePrintable.printTreeOrToString(
+                            value.get(),
+                            printer
+                    );
+
+                    printer.lineStart();
+                }
+                printer.outdent();
+            }
+        }
+    }
+}
---
+++ src/test/java/walkingkooka/tree/text/TextStylePropertyTest.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.text;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.ToStringTesting;
+import walkingkooka.color.Color;
+import walkingkooka.naming.HasNameTesting;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.text.printer.TreePrintableTesting;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class TextStylePropertyTest implements ClassTesting<TextStyleProperty<TextAlign>>,
+        HashCodeEqualsDefinedTesting2<TextStyleProperty<TextAlign>>,
+        HasNameTesting<TextStylePropertyName<TextAlign>>,
+        ToStringTesting<TextStyleProperty<TextAlign>>,
+        TreePrintableTesting {
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullNameFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> TextStyleProperty.with(
+                        null,
+                        Optional.of(
+                                TextAlign.CENTER
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullValueFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> TextStyleProperty.with(
+                        TextStylePropertyName.TEXT_ALIGN,
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final TextStylePropertyName<TextAlign> name = TextStylePropertyName.TEXT_ALIGN;
+        final Optional<TextAlign> value = Optional.of(
+                TextAlign.CENTER
+        );
+
+        final TextStyleProperty<TextAlign> property = TextStyleProperty.with(
+                name,
+                value
+        );
+
+        this.nameAndCheck(
+                property,
+                name
+        );
+
+        this.checkEquals(
+                value,
+                property.value(),
+                "value"
+        );
+    }
+
+    // equals...........................................................................................................
+
+    @Test
+    public void testEqualsDifferentName() {
+        this.checkNotEquals(
+                TextStyleProperty.with(
+                        TextStylePropertyName.COLOR,
+                        Optional.of(
+                                Color.BLACK
+                        )
+                ),
+                TextStyleProperty.with(
+                        TextStylePropertyName.BACKGROUND_COLOR,
+                        Optional.of(
+                                Color.BLACK
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentValue() {
+        this.checkNotEquals(
+                TextStyleProperty.with(
+                        TextStylePropertyName.COLOR,
+                        Optional.of(
+                                Color.BLACK
+                        )
+                ),
+                TextStyleProperty.with(
+                        TextStylePropertyName.COLOR,
+                        Optional.of(
+                                Color.WHITE
+                        )
+                )
+        );
+    }
+
+    @Override
+    public TextStyleProperty<TextAlign> createObject() {
+        return TextStyleProperty.with(
+                TextStylePropertyName.TEXT_ALIGN,
+                Optional.of(
+                        TextAlign.CENTER
+                )
+        );
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                TextStyleProperty.with(
+                        TextStylePropertyName.TEXT_ALIGN,
+                        Optional.of(
+                                TextAlign.CENTER
+                        )
+                ),
+                "text-align=CENTER"
+        );
+    }
+
+    // TreePrintable....................................................................................................
+
+    @Test
+    public void testTreePrint() {
+        this.treePrintAndCheck(
+                TextStyleProperty.with(
+                        TextStylePropertyName.TEXT_ALIGN,
+                        Optional.of(
+                                TextAlign.CENTER
+                        )
+                ),
+                "text-align\n" +
+                        "  CENTER\n"
+        );
+    }
+
+    @Test
+    public void testTreePrintEmptyValue() {
+        this.treePrintAndCheck(
+                TextStyleProperty.with(
+                        TextStylePropertyName.TEXT_ALIGN,
+                        Optional.empty()
+                ),
+                "text-align\n"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TextStyleProperty<TextAlign>> type() {
+        return Cast.to(TextStyleProperty.class);
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
