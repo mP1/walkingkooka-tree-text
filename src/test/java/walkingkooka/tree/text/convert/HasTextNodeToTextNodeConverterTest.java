ceefb05db233f0b2e9a2f72e378cf150e79b8b0a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-22 12:36:23 +1000

    HasTextNodeToTextNodeConverter

---
+++ src/test/java/walkingkooka/tree/text/convert/HasTextNodeToTextNodeConverterTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.text.convert;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.ToStringTesting;
+import walkingkooka.convert.ConverterContexts;
+import walkingkooka.convert.ConverterTesting2;
+import walkingkooka.convert.FakeConverterContext;
+import walkingkooka.tree.text.HasTextNode;
+import walkingkooka.tree.text.TextNode;
+
+public final class HasTextNodeToTextNodeConverterTest implements ConverterTesting2<HasTextNodeToTextNodeConverter<FakeConverterContext>, FakeConverterContext>,
+    ToStringTesting<HasTextNodeToTextNodeConverter<FakeConverterContext>> {
+
+    @Test
+    public void testStringToString() {
+        this.convertFails(
+            "hello",
+            String.class
+        );
+    }
+
+    @Test
+    public void testTextNodeToTextNodeFails() {
+        this.convertFails(
+            TextNode.text("hello"),
+            TextNode.class
+        );
+    }
+
+    @Test
+    public void testNullHasTextNodeToTextNode() {
+        this.convertAndCheck(
+            (HasTextNode) null,
+            TextNode.class,
+            null
+        );
+    }
+
+    @Test
+    public void testHasTextNodeToTextNode() {
+        final TextNode textNode = TextNode.text("hello");
+
+        this.convertAndCheck(
+            new HasTextNode() {
+                @Override
+                public TextNode toTextNode() {
+                    return textNode;
+                }
+            },
+            TextNode.class,
+            textNode
+        );
+    }
+
+    @Override
+    public HasTextNodeToTextNodeConverter<FakeConverterContext> createConverter() {
+        return HasTextNodeToTextNodeConverter.instance();
+    }
+
+    @Override
+    public FakeConverterContext createContext() {
+        return (FakeConverterContext) ConverterContexts.fake();
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+            this.createConverter(),
+            "HasTextNode to TextNode"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<HasTextNodeToTextNodeConverter<FakeConverterContext>> type() {
+        return Cast.to(HasTextNodeToTextNodeConverter.class);
+    }
+}
