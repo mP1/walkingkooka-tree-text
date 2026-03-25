f7dea3d0993fef95d1e7586c7a9b87c2bdf5336f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-25 20:25:15 +1100

    InvalidTextStylePropertyNameException

---
+++ src/main/java/walkingkooka/tree/text/InvalidTextStylePropertyNameException.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.text;
+
+import java.util.Objects;
+
+/**
+ * Reports an invalid {@link TextStylePropertyName} such as passing a padding to a {@link Border}.
+ */
+public final class InvalidTextStylePropertyNameException extends IllegalArgumentException {
+
+    private static final long serialVersionUID = 1L;
+
+    public static InvalidTextStylePropertyNameException border(final TextStylePropertyName<?> property) {
+        return new InvalidTextStylePropertyNameException(
+            "border",
+            property
+        );
+    }
+
+    public static InvalidTextStylePropertyNameException margin(final TextStylePropertyName<?> property) {
+        return new InvalidTextStylePropertyNameException(
+            TextStylePropertyName.MARGIN.name,
+            property
+        );
+    }
+
+    public static InvalidTextStylePropertyNameException padding(final TextStylePropertyName<?> property) {
+        return new InvalidTextStylePropertyNameException(
+            TextStylePropertyName.PADDING.name,
+            property
+        );
+    }
+
+    public InvalidTextStylePropertyNameException(final String filter,
+                                                 final TextStylePropertyName<?> property) {
+        super();
+        this.filter = Objects.requireNonNull(filter, "filter");
+        this.propertyName = Objects.requireNonNull(property, "propertyName");
+    }
+
+    @Override
+    public String getMessage() {
+        return "Invalid " + this.filter + " property " + propertyName.inQuotes();
+    }
+
+    public String filter() {
+        return this.filter;
+    }
+
+    private final String filter;
+
+    public TextStylePropertyName<?> propertyName() {
+        return this.propertyName;
+    }
+
+    private final TextStylePropertyName<?> propertyName;
+}
---
+++ src/test/java/walkingkooka/tree/text/InvalidTextStylePropertyNameExceptionTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.text;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.reflect.ThrowableTesting2;
+
+public final class InvalidTextStylePropertyNameExceptionTest implements ThrowableTesting2<InvalidTextStylePropertyNameException> {
+
+    @Test
+    public void testGetMessageBorder() {
+        this.checkMessage(
+            InvalidTextStylePropertyNameException.border(TextStylePropertyName.COLOR),
+            "Invalid border property \"color\""
+        );
+    }
+
+    @Test
+    public void testGetMessageMargin() {
+        this.checkMessage(
+            InvalidTextStylePropertyNameException.margin(TextStylePropertyName.COLOR),
+            "Invalid margin property \"color\""
+        );
+    }
+
+    @Test
+    public void testGetMessagePadding() {
+        this.checkMessage(
+            InvalidTextStylePropertyNameException.padding(TextStylePropertyName.COLOR),
+            "Invalid padding property \"color\""
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public void testIfClassIsFinalIfAllConstructorsArePrivate() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Class<InvalidTextStylePropertyNameException> type() {
+        return InvalidTextStylePropertyNameException.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
